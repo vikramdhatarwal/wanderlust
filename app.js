a898078b12d414fd2f9541f3044310fd1175356a
Vikram Dhatarwal <vikk.choudhary06@gmail.com>
2026-06-20 11:18:03 +0530

    Edit route to display a form for editing an existing listing and Update route to handle form submission and update an existing listing

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const ejs=require("ejs");
 const Listing=require("./models/listing");
 const PORT=3000;
 app.use(express.urlencoded({extended:true}));
+const methodOverride=require("method-override");
+app.use(methodOverride("_method"));
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
@@ -76,6 +78,35 @@ app.get("/listings/:id",async(req,res)=>{
     }
 });
 
+//Edit route to display a form for editing an existing listing
+app.get("/listings/:id/edit",async(req,res)=>{
+    const {id}=req.params;
+    try{
+        const listing=await Listing.findById(id);
+        if(!listing){
+            return res.status(404).send("Listing not found");
+        }
+        res.render("listings/edit.ejs",{listing});
+    }catch(err){
+        console.error("Error fetching listing:", err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+//Update route to handle form submission and update an existing listing
+app.put("/listings/:id",async(req,res)=>{
+    const {id}=req.params;
+    try{
+        const updatedListing=await Listing.findByIdAndUpdate(id,req.body.listing,{new:true});
+        if(!updatedListing){
+            return res.status(404).send("Listing not found");
+        }
+        res.redirect(`/listings/${updatedListing._id}`);
+    }catch(err){
+        console.error("Error updating listing:", err);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 // app.get("/testlistings",async(req,res)=>{ 
 //     let sampleListing=new Listing({
