d91ad3af47871ee76075a3a76264635e259153b7
Vikram Dhatarwal <vikk.choudhary06@gmail.com>
2026-06-21 00:20:25 +0530

    Added Home.ejs forhomepage and some more styling

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use (express.static(path.join(__dirname,"/public")));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.get("/",(req,res)=>{
-    res.send("Hi,I am root");
+    res.render("home.ejs");
 });
 
 
