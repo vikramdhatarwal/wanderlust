25b28bf01ac4eca3fd71b4ee4e854f36ac58bb7f
Vikram Dhatarwal <vikk.choudhary06@gmail.com>
2026-06-20 08:31:56 +0530

    Initialize Database

---
+++ init/index.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const initData=require('./data.js');
+const listing = require('../models/listing.js');
+
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+main().catch((err)=>{
+    console.error("Error initializing database:", err);
+});
+
+async function main(){
+    try {
+        await mongoose.connect(MONGO_URL);
+        console.log("Connected to MongoDB");
+
+        await listing.deleteMany({});
+        console.log("Existing listings deleted.");
+        await listing.insertMany(initData.data);
+        console.log("New listings inserted.");
+    } catch (err) {
+        throw err;
+    } finally {
+        await mongoose.connection.close();
+        console.log("Database initialization complete.");
+    }
+}
